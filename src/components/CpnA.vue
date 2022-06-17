cbb3cee5cc558fae131b6319aadb712c300512ef
lanslorin <70133718+lanslorin@users.noreply.github.com>
2022-06-17 17:03:46 +0800

    Update CpnA.vue

--- src/components/CpnA.vue
+++ src/components/CpnA.vue
@@ -1,18 +1,35 @@
 <script setup>
 import { useList } from '../store';
 const list = useList();
+const add = () => {
+  // list.counts++
+  list.addCount('777')
+  // list.$patch({ counts: list.counts + 1 })
+};
+const reset = () => {
+  list.$reset()
+}
 </script>
 
 <template>
   <div class="cpn-a">
     <div>组件A</div>
     <div class="list">
-      <div>列表</div>
+      <div>state列表</div>
       <div class="item" v-for="item in list.list">
         {{ item.name }} - {{ item.age }} - {{ item.gender === 1 ? '男' : '女' }}
       </div>
+      <div>getters列表</div>
+      <div class="item" v-for="item in list.listFiltered">
+        {{ item.name }} - {{ item.age }} - {{ item.gender === 1 ? '男' : '女' }}
+      </div>
+      <div>getters</div>
+      <div>{{list.doubleCount}}</div>
+      <div>{{list.sixTimeCount}}</div>
     </div>
-    <button>click</button>
+    <div style="color:red">List.count:{{list.counts}}</div>
+    <button @click="add">click</button>
+    <button @click="reset">reset</button>
   </div>
 </template>
 
