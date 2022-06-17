adc49159ef8c12e4a13e5d16edc185057fc73ba4
lanslorin <70133718+lanslorin@users.noreply.github.com>
2022-06-17 17:04:10 +0800

    Update CpnB.vue

--- src/components/CpnB.vue
+++ src/components/CpnB.vue
@@ -1,6 +1,11 @@
 <script setup>
 import { useCount } from '../store';
-const { count, addCount } = useCount();
+import {storeToRefs} from 'pinia'
+import { useList } from '../store';
+const list = useList();
+const counter = useCount()
+const {count} = storeToRefs(counter)
+const {addCount} = counter
 const add = () => {
   addCount();
 };
@@ -9,7 +14,8 @@ const add = () => {
 <template>
   <div class="cpn-b">
     <div>组件B</div>
-    <div>Count:{{ count }}</div>
+    <div>Count.count:{{ count }}</div>
+    <div style="color:red">List.count:{{ list.counts }}</div>
     <button @click="add">click</button>
   </div>
 </template>
@@ -17,6 +23,6 @@ const add = () => {
 <style scoped>
 .cpn-b {
   height: 300px;
-  background-color: orange;
+  background-color: rgb(194, 140, 230);
 }
 </style>
